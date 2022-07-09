8e7cc8fe6d96c561ee42bf04e85ba582d283b3be
parkjungyoon <tbr06057@naver.com>
2022-07-09 12:36:13 +0900

    2606번 복습

--- Baekjoon/Graphs/2606_바이러스.py
+++ Baekjoon/Graphs/2606_바이러스.py
@@ -34,3 +34,40 @@ def dfs(v):
 dfs(1)
 result = len([i for i in node if i])
 print(result-1)
+
+
+# < 복습 >
+
+# from collections import deque
+
+# n = int(input())
+# m = int(input())
+# graph = [[0]*(n+1) for _ in range(n+1)]
+# visited = [False] * (n+1)
+
+# for _ in range(m):
+#     x, y = map(int,input().split())
+#     graph[x][y] = graph[y][x] = 1
+
+# # dfs 풀이방법
+# def dfs(v):
+#     visited[v] = True
+#     for i in range(1,n+1):
+#         if graph[v][i] == 1 and not visited[i]:
+#             visited[i] = True
+#             dfs(i)
+
+# # bfs 풀이방법
+# # def bfs(v):
+# #     queue = deque([v])
+# #     visited[v] = True
+# #     while queue:
+# #         v = queue.popleft()
+# #         for i in range(1,n+1):
+# #             if graph[v][i] == 1 and not visited[i]:
+# #                 visited[i] = True
+# #                 queue.append(i)
+
+# dfs(1)
+# # bfs(1)
+# print(len([i for i in visited if i])-1)
