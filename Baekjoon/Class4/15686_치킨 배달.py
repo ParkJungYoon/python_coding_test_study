5c6eab6392970ec800ac7183d9bcdec5292d237d
parkjungyoon <tbr06057@naver.com>
2022-10-09 23:20:09 +0900

    15686 치킨 배달 리팩토링 흔적

--- Baekjoon/Class4/15686_치킨 배달.py
+++ Baekjoon/Class4/15686_치킨 배달.py
@@ -1,21 +1,23 @@
 # 여기서는 좌표 1부터 시작
 # 반복을 돌면서 1에서 bfs해서 2를 발견하면 그 거리 구하고 -> 응. 이거 아니야
 
-import sys
+from sys import stdin
 from itertools import combinations
 
-input = sys.stdin.readline
+input = stdin.readline
 n, m = map(int, input().split())
-graph = [list(map(int,input().split())) for _ in range(n)]
+# 전체 graph를 받아올 필요가 없음.
+# graph = [list(map(int,input().split())) for _ in range(n)]
 
 # 폐업시키지 않을 치킨집을 최대 M개를 골랐을 때, -> 아? 경우의 수?
 
 house, chicken = [], []
 for i in range(n):
+    graph = list(map(int,input().split()))
     for j in range(n):
-        if graph[i][j] == 1:
+        if graph[j] == 1:
             house.append((i+1,j+1))
-        elif graph[i][j] == 2:
+        elif graph[j] == 2:
             chicken.append((i+1,j+1))
 
 # print(house)
@@ -25,15 +27,20 @@ def distance(a,b):
     distance = abs(a[0]-b[0]) + abs(a[1]-b[1])
     return distance
 
+# float("inf")
 min_distance = 100000
-sum_distance = 0
+# sum_distance = 0
 
 for chi in combinations(chicken, m):
+    sum_distance = 0
     for hou in house:
 # 않이...여기서... 또 완전탐색..? 이거 맞아?
         sum_distance += min([distance(hou, i) for i in chi])
-    min_distance = min(min_distance, sum_distance)
-    sum_distance = 0
+    # min_distance = min(min_distance, sum_distance)
+    # 새로 구한 거리 합이 더 작으면 min 값을 업데이트
+    if sum_distance < min_distance:
+            min_distance = sum_distance
+    # sum_distance = 0
 
 print(min_distance)
 
