bb984f61ad765f3efd12c6782a15e9f136994df1
parkjungyoon <tbr06057@naver.com>
2022-11-04 19:06:23 +0900

    [백준, 3190] 뱀 : 리팩토링

--- Baekjoon/DataStructure/3190_뱀.py
+++ Baekjoon/DataStructure/3190_뱀.py
@@ -1,3 +1,71 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n  = int(input())
+k = int(input())
+graph = [[0]*(n+1) for _ in range(n+1)]
+
+for i in range(k):
+    row, col = map(int, input().split())
+    graph[row][col] = 1
+
+l = int(input())
+direction = deque([])
+
+for i in range(l):
+    x, c = input().split()
+    direction.append((int(x), c))
+
+print(direction)
+# 현재 위치를 계속 추가하다가
+
+# 시작 방향
+# dx, dy = 0, 1
+dx, dy = (0, 1, 0, -1), (1, 0, -1, 0)
+cur = 0
+
+def game(x, y):
+    global cur
+    queue = deque([(x, y)])
+
+    count = 0
+    while True:
+        count += 1
+
+        x, y = queue[-1][0], queue[-1][1]
+        nx, ny = x + dx[cur], y + dy[cur]
+        # 머리 돌리는 조건
+        if direction:
+            if count == direction[0][0]:
+                changeDirection(direction[0][1])
+                direction.popleft()
+
+        # 종료 조건
+        if nx < 1 or nx > n or ny < 1 or ny > n:
+            return count
+        if (nx, ny) in queue:
+            return count
+
+        # 이동한 곳에 사과 있으면 다시 append
+        if graph[nx][ny] == 1:
+            graph[nx][ny] = 0
+            queue.append((nx, ny))
+        else:
+            queue.popleft()
+            queue.append((nx, ny))
+
+
+def changeDirection(dir):
+    global cur
+    if dir == 'L':
+        cur = (cur - 1) % 4
+    else:
+        cur = (cur + 1) % 4
+
+print(game(1, 1))
+
+
 '''
 from collections import deque
 
@@ -95,75 +163,4 @@ def changeDirection(dir):
 
 print(game(1, 1))
 '''
-
-import sys
-from collections import deque
-input = sys.stdin.readline
-
-n  = int(input())
-k = int(input())
-graph = [[0]*(n+1) for _ in range(n+1)]
-
-for i in range(k):
-    row, col = map(int, input().split())
-    graph[row][col] = 1
-graph[1][1] = 2
-
-l = int(input())
-direction = []
-
-for i in range(l):
-    x, c = input().split()
-    direction.append((int(x), c))
-
-
-# 현재 위치를 계속 추가하다가
-
-# 시작 방향
-# dx, dy = 0, 1
-dx, dy = (0, 1, 0, -1), (1, 0, -1, 0)
-cur = 0
-
-def game(x, y):
-    global cur
-    queue = deque([(x, y)])
-
-    count = 0
-    while True:
-        count += 1
-
-        x, y = queue[-1][0], queue[-1][1]
-        nx, ny = x + dx[cur], y + dy[cur]
-        # 머리 돌리는 조건
-        if direction:
-            if count == direction[0][0]:
-                changeDirection(direction[0][1])
-                direction.pop(0)
-
-        # 종료 조건
-        if nx < 1 or nx > n or ny < 1 or ny > n:
-            return count
-        if (nx, ny) in queue:
-            return count
-
-        # 이동한 곳에 사과 있으면 다시 append
-        if graph[nx][ny] == 1:
-            graph[nx][ny] = 0
-            queue.append((nx, ny))
-        else:
-            queue.popleft()
-            queue.append((nx, ny))
-            # x, y = queue.popleft()
-            # 현재 위치를 업데이트를
-
-
-def changeDirection(dir):
-    global cur
-    if dir == 'L':
-        cur = (cur - 1) % 4
-    else:
-        cur = (cur + 1) % 4
-
-print(game(1, 1))
-
 
