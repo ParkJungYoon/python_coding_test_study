14d17e6f898360b48df9be34599cb0cf74a14797
ParkJungYoon <tbr06057@naver.com>
2022-12-30 14:50:06 +0900

    [백준, 11399] ATM / 복습

---
+++ Baekjoon/Review/11399_ATM.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+p = list(map(int, input().split()))
+
+p.sort()
+
+result = 0
+for i in range(n):
+    result += p[i] * (n-i)
+
+print(result)
