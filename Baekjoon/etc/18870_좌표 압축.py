bb2b30641ef1e133946fe7e6344a8d8176dfac6c
ParkJungYoon <tbr06057@naver.com>
2023-01-23 23:04:25 +0900

    refactor: 딕셔너리 컴프리헨션으로 코드 수정

--- Baekjoon/etc/18870_좌표 압축.py
+++ Baekjoon/etc/18870_좌표 압축.py
@@ -2,9 +2,10 @@ n = int(input())
 x = list(map(int, input().split()))
 
 new_x = sorted(list(set(x)))
-dic = {}
-for i in range(len(new_x)):
-    dic[new_x[i]] = i
+# dic = {}
+# for i in range(len(new_x)):
+#     dic[new_x[i]] = i
+dic = {new_x[i] : i for i in range(len(new_x))}
 
 result = []
 for j in x:
