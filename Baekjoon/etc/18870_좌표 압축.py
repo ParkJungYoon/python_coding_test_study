fbc16209e118c325085dc3958a3f77dfdaa4a353
ParkJungYoon <tbr06057@naver.com>
2023-01-23 23:02:39 +0900

    [백준, 18870] 좌표 압축

---
+++ Baekjoon/etc/18870_좌표 압축.py
@@ -0,0 +1,13 @@
+n = int(input())
+x = list(map(int, input().split()))
+
+new_x = sorted(list(set(x)))
+dic = {}
+for i in range(len(new_x)):
+    dic[new_x[i]] = i
+
+result = []
+for j in x:
+    result.append(dic[j])
+
+print(*result)
