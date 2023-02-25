e57acd2d049cac25b2fc0058dd7f40ac57f9639c
ParkJungYoon <tbr06057@naver.com>
2023-02-25 18:09:46 +0900

    refactor: [백준, 15649] in 연산자 대신 방문여부 체크로 로직 변경

--- Baekjoon/Backtracking/15649_N과 M (1).py
+++ Baekjoon/Backtracking/15649_N과 M (1).py
@@ -1,9 +1,31 @@
 n, m = map(int, input().split())
 arr = []
+visited = [False] * (n+1)
  
 def dfs():
     if len(arr) == m:
-        print(*arr)
+        print(*arr) 
+        return
+    
+    for i in range(1, n+1):
+        if not visited[i]:
+            arr.append(i)
+            visited[i] = True
+            dfs()
+            arr.pop()
+            visited[i] = False
+
+dfs()
+
+'''
+# in 연산은 O(N) 걸리기 때문에 로직 살짝 바꾸자!
+
+n, m = map(int, input().split())
+arr = []
+ 
+def dfs():
+    if len(arr) == m:
+        print(*arr) 
         return
     
     for i in range(1, n+1):
@@ -13,3 +35,4 @@ def dfs():
             arr.pop()
 
 dfs()
+'''
