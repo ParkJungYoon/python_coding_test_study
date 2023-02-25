98d7c286472e9ceb3eaf4c8a4fcb64fc40d0fa52
ParkJungYoon <tbr06057@naver.com>
2023-02-25 18:24:12 +0900

    add: 주석 추가

--- Baekjoon/Backtracking/15649_N과 M (1).py
+++ Baekjoon/Backtracking/15649_N과 M (1).py
@@ -1,10 +1,14 @@
+# 순열 : 백트래킹으로 O(N)으로 풀기
+
+# from itertools import permutations
+
 n, m = map(int, input().split())
 arr = []
 visited = [False] * (n+1)
  
 def dfs():
     if len(arr) == m:
-        print(*arr) 
+        print(*arr)
         return
     
     for i in range(1, n+1):
@@ -16,6 +20,9 @@ def dfs():
             visited[i] = False
 
 dfs()
+
+# for permu in permutations(range(1,n+1), m):
+#     print(*permu)
 
 '''
 # in 연산은 O(N) 걸리기 때문에 로직 살짝 바꾸자!
