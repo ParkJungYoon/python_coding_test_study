7abe2971854bf7ae56bfc6d6987e31d62b8ad7e8
ParkJungYoon <tbr06057@naver.com>
2023-03-14 11:30:18 +0900

    [백준, 5972] 택배 배송

---
+++ Baekjoon/etc/5972_택배 배송.py
@@ -0,0 +1,33 @@
+# 최소 비용 최단 경로 -> 다익스트라
+import sys
+import heapq
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+distance = [INF] * (n+1)
+ 
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b,c))
+    graph[b].append((a,c))
+
+def delivery(start):
+    temp = []
+    heapq.heappush(temp, (0, start))
+    distance[start] = 0
+
+    while temp:
+        dist, now = heapq.heappop(temp)
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(temp, (cost, i[0]))
+
+delivery(1)
+print(distance[n])
