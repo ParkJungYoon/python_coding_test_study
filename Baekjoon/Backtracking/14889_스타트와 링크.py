086a54459cd740cb8d01bbd54861e76fe40a03c0
ParkJungYoon <tbr06057@naver.com>
2023-04-02 13:48:46 +0900

    refactor: 조합에서 백트래킹 방법으로 재구현

--- Baekjoon/Backtracking/14889_스타트와 링크.py
+++ Baekjoon/Backtracking/14889_스타트와 링크.py
@@ -1,3 +1,37 @@
+# 풀이2 : 백트래킹 제대로 쓰기. 시간: 5420ms
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+s = [list(map(int, input().split())) for _ in range(n)]
+visited = [False for _ in range(n)]
+answer = sys.maxsize
+
+def dfs(dep, idx):
+    global answer
+    if dep == n//2:
+        start = 0
+        link = 0
+        for x in range(n):
+            for y in range(n):
+                if visited[x] and visited[y]:
+                    start += s[x][y]
+                elif not visited[x] and not visited[y]:
+                    link += s[x][y]
+        answer = min(answer, abs(start-link))
+        return
+    
+    for i in range(idx,n):
+        visited[i] = True
+        dfs(dep+1, i+1)
+        visited[i] = False
+
+dfs(0,0)     
+print(answer)
+
+'''
+풀이 1 : 시간 효율이 너무 안좋은 것 같음. 빡구현. 시간: 3144ms
 import sys
 input = sys.stdin.readline
 from itertools import combinations
@@ -35,7 +69,7 @@ for i in range(count):
     answer = min(answer, abs(start-link))
 
 print(answer)
-
+'''
 
 
 '''
