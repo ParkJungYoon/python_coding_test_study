327675ddd62933cd78ea18c5b5434c788dba87ad
ParkJungYoon <tbr06057@naver.com>
2023-08-10 12:14:06 +0900

    refactor: 그냥 뒤집는 방식에서 변경

--- Baekjoon/Class2/1259_팰린드롬수.py
+++ Baekjoon/Class2/1259_팰린드롬수.py
@@ -1,13 +1,22 @@
 import sys
 
+def check(num):
+    if len(num) == 1:
+        return True
+    for i in range(len(num)//2):
+        if num[i] != num[-(i+1)]:
+            return False
+    return True
+
+# 그냥 뒤집는거 보다 빠름
 while True:
     num = sys.stdin.readline().strip()
-    reverse_num = num[::-1]
+    # reverse_num = num[::-1]
 
     if int(num) == 0:
         break
 
-    if num == reverse_num:
+    if check(num):
         print('yes')
     else:
         print('no')
