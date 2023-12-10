82f9d7e5da27aab93383d0fdf60f569abf43c465
ParkJungYoon <tbr06057@naver.com>
2023-12-10 22:43:38 +0900

    [백준, 1158] 요세푸스 문제

--- Baekjoon/DataStructure/1158_요세푸스 문제.py
+++ Baekjoon/DataStructure/1158_요세푸스 문제.py
@@ -1,6 +1,23 @@
 from collections import deque
 
 n, k = map(int, input().split())
+queue = deque(range(1, n + 1))
+result = []
+
+while queue:
+    for _ in range(k-1):
+        queue.append(queue.popleft())
+    result.append(str(queue.popleft()))
+
+print("<" + ", ".join(result) + ">")
+
+
+# 시간 오래걸림. 6524ms
+
+'''
+from collections import deque
+
+n, k = map(int, input().split())
 queue = deque(range(1, n+1))
 count = 0
 result = []
@@ -15,3 +32,4 @@ while queue:
         result.append(str(current))
 
 print("<" + ", ".join(result) + ">")
+'''
