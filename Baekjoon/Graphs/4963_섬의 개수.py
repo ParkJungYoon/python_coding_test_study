e112679c61c77c0f6de0dd6e5af5b48b4356b4a6
ParkJungYoon <tbr06057@naver.com>
2023-12-25 14:26:43 +0900

    [백준, 4963] 섬의 개수

---
+++ Baekjoon/Graphs/4963_섬의 개수.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+dx, dy = (1, 1, 0, -1, -1, -1, 0, 1), (0, 1, 1, 1, 0, -1, -1, -1)
+
+def bfs(x, y):
+    queue = deque([(x, y)])
+    graph[x][y] = 0
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(8):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] == 1:
+                queue.append((nx, ny))
+                graph[nx][ny] = 0
+
+while True:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
+    graph = [list(map(int, input().split())) for _ in range(h)]
+
+    count = 0
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == 1:
+                bfs(i, j)
+                count += 1
+    print(count)
