057bd7b9c9db0486d3681ae3e5434db554162840
ParkJungYoon <tbr06057@naver.com>
2024-01-31 08:54:39 +0900

    [백준, 12865] 평범한 배낭

--- Baekjoon/DP/12865_평범한 배낭.py
+++ Baekjoon/DP/12865_평범한 배낭.py
@@ -21,14 +21,29 @@ dp = [[0] * (n+1) for _ in range(k+1)]
 # i는 물품 번호 (1~n번까지)
 for i in range(1, n+1):
     w, v = map(int, input().split())
+
     # j는 무게 (k가 최대 공간, 내가 3만큼 넣으면 남은 공간은 4)
-    if w > k:
-        for jj in range(1,k+1):
-            dp[jj][i] = dp[jj][i-1]
-    for j in range(w, k+1):
+    for j in range(k+1):
         # 현재 배낭의 최대 무게는 j이다.
         # j-w가 남은 배낭의 공간
-        dp[j][i] = max(dp[j][i-1], v + dp[j-w][i-1])
+        if w > j:
+            dp[j][i] = dp[j][i-1]
+        else:
+            dp[j][i] = max(dp[j][i-1], v + dp[j-w][i-1])
+
+    '''
+    틀린 이유: 물건의 크기가 배낭의 크기보다 큰 경우는 그냥 넘어가서 틀림.
+    왜냐하면 다음 인덱스 검색 때 그 직전의 값을 기준으로 맥스멈 값을 계산하는데
+    그 앞 단 인덱스를 계산 안하고 넘어가면 0이기 때문에!
+    '''
+    # j는 무게 (k가 최대 공간, 내가 3만큼 넣으면 남은 공간은 4)
+    # if w > k:
+    #     for jj in range(1,k+1):
+    #         dp[jj][i] = dp[jj][i-1]
+    # for j in range(w, k+1):
+    #     # 현재 배낭의 최대 무게는 j이다.
+    #     # j-w가 남은 배낭의 공간
+    #     dp[j][i] = max(dp[j][i-1], v + dp[j-w][i-1])
 
 print(dp[-1][-1])
 
