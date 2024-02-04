4a68a51b8291ea8e2dba212920f23d942e1f6b44
ParkJungYoon <tbr06057@naver.com>
2024-02-04 15:14:40 +0900

    refactor: 시간이 훨씬 빨라짐

--- Baekjoon/TwoPointer/2531_회전 초밥.py
+++ Baekjoon/TwoPointer/2531_회전 초밥.py
@@ -10,7 +10,8 @@ answer = 0
 
 def calc_number_of_things():
     cur_cobab[c] += 1
-    number = len([i for i in list(cur_cobab.values()) if i != 0])
+    # number = len([i for i in list(cur_cobab.values()) if i != 0])
+    number = len(list(cur_cobab.values()))
     cur_cobab[c] -= 1
     return number
 
@@ -21,6 +22,9 @@ for i in range(k,n+k):
     answer = max(answer,calc_number_of_things())
     i = i % n
     cur_cobab[cobab[i-k]] -= 1
+    # 초밥이 0개인 경우를 바로바로 지워주니깐 시간이 2736ms -> 668ms로 바뀜
+    if cur_cobab[cobab[i-k]] == 0:
+        del cur_cobab[cobab[i-k]]
     cur_cobab[cobab[i]] += 1
 
 print(answer)
