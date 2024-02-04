2453cfcea429435f27b1bd32da3b5b49c4dca252
ParkJungYoon <tbr06057@naver.com>
2024-02-04 14:56:59 +0900

    [백준, 2531] 회전 초밥

---
+++ Baekjoon/TwoPointer/2531_회전 초밥.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+from collections import defaultdict
+
+n, d, k, c = map(int, input().split())
+cobab = [int(input()) for _ in range(n)]
+check_coupon = False
+cur_cobab = defaultdict(int)
+answer = 0
+
+def calc_number_of_things():
+    cur_cobab[c] += 1
+    number = len([i for i in list(cur_cobab.values()) if i != 0])
+    cur_cobab[c] -= 1
+    return number
+
+for i in range(k):
+    cur_cobab[cobab[i]] += 1
+
+for i in range(k,n+k):
+    answer = max(answer,calc_number_of_things())
+    i = i % n
+    cur_cobab[cobab[i-k]] -= 1
+    cur_cobab[cobab[i]] += 1
+
+print(answer)
