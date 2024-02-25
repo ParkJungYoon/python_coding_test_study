54b33b7a6c1bfd226deae15bebab14e19654a563
ParkJungYoon <tbr06057@naver.com>
2024-02-25 14:55:32 +0900

    [백준, 1021] 회전하는 큐

---
+++ Baekjoon/DataStructure/1021_회전하는 큐.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+'''
+34567812 길이 8이니깐
+
+6의 위치는 6-3=3 : 앞에 3개를 빼야함 vs 뒤에 (8-(3+1)=4)
+2의 위치는 2-3 : -1
+7의 위치는 7-3=4
+'''
+
+cursor = 0
+queue = deque(range(1,n+1))
+answer = 0
+
+for cursor in range(m):
+    check = queue[0]
+    idx = queue.index(a[cursor])
+
+    if idx == 0:
+        queue.popleft()
+    elif idx < 0:
+        for i in range(abs(idx)):
+            queue.appendleft(queue.pop())
+        answer += (abs(idx)+1)
+        queue.pop()
+    elif idx > 0:
+        if idx > (len(queue)-1-idx):
+            for i in range(len(queue)-1-idx):
+                queue.appendleft(queue.pop())
+            answer += len(queue)-idx
+            queue.pop()
+        else:
+            for i in range(idx):
+                queue.append(queue.popleft())
+            answer += idx
+            queue.popleft()
+
+'''
+deque([3, 4, 5, 6, 7, 8, 9, 10, 1])
+deque([10, 1, 3, 4, 5, 6, 7, 8])
+1 + 3 + 4
+'''
+
+print(answer)
